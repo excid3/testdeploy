ec6579877bce69a3bf40e48a1b5768c28f86534c
Chris Oliver <excid3@gmail.com>
2013-10-28 14:15:01 -0500

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GorailsTest::Application.config.secret_key_base = '12f32be72ac419c26dc1eebd386f65131b3158c9dfba6ce3e58b0818827bb4d07f1ef14b48e751a2b5f387ea99317801996180f3e5f3d719eb1b535931046392'
